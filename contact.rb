876a166d0915f7223c3b5458b76467056db68da0
Rahul Parmar <rahul.parmar@gmail.com>
2014-01-03 10:11:54 -0500

    Pushing AR project

--- contact.rb
+++ contact.rb
@@ -1,18 +1,3 @@
-class Contact
-  
-  attr_accessor :first_name
-  attr_accessor :last_name
-  attr_accessor :email
-  attr_accessor :phonenumber
-  
-  def initialize(name, email, *phonenumber = 0)
-    # TODO: assign local variables to instance variables
-    @name = name
-    @email = email
-  end
-  
-  def to_s
-    # TODO: return string representation of Contact
-  end
-  
+require 'active_record'
+class Contact < ActiveRecord::Base  
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 require_relative 'contact'
 require_relative 'application'
+require_relative 'database.rb'
+require 'active_record'
 
 Application.new.run
